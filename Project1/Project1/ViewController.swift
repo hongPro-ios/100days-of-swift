7c5669cf250c089c13c92f01044b45eafd6d8877
hongPro-ios <hongjeongseob@icloud.com>
2021-06-24 10:19:33 +0900

    Add iOS - Project 1, part one
    Setting up
    Listing images with FileManager
    Designing our interface

---
+++ Project1/Project1/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by JEONGSEOB HONG on 2021/06/24.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    var pictures = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let fileManager = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fileManager.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
+        print(pictures)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        
+        return cell
+    }
+}
+
