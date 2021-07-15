4302e5a96262e20bf923700b320b1b5c3ce5c1f1
hongPro-ios <hongjeongseob@icloud.com>
2021-07-15 11:40:40 +0900

    Add Project 8, part two about "adding button target"

--- Project8/Project8/ViewController.swift
+++ Project8/Project8/ViewController.swift
@@ -16,6 +16,16 @@ class ViewController: UIViewController {
     var clearButton: UIButton!
     var letterButtons = [UIButton]()
     
+    var activatedButtons = [UIButton]()
+    var solutions = [String]()
+    
+    var score = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(score)"
+        }
+    }
+    var level = 1
+    
     override func loadView() {
         view = UIView()
         view.backgroundColor = .white
@@ -54,11 +64,13 @@ class ViewController: UIViewController {
         submitButton = UIButton(type: .system)
         submitButton.translatesAutoresizingMaskIntoConstraints = false
         submitButton.setTitle("SUBMIT", for: .normal)
+        submitButton.addTarget(self, action: #selector(submitTapped), for: .touchUpInside)
         view.addSubview(submitButton)
         
         clearButton = UIButton(type: .system)
         clearButton.translatesAutoresizingMaskIntoConstraints = false
         clearButton.setTitle("CLEAR", for: .normal)
+        clearButton.addTarget(self, action: #selector(clearTapped), for: .touchUpInside)
         view.addSubview(clearButton)
         
         let buttonsView = UIView()
@@ -106,31 +118,122 @@ class ViewController: UIViewController {
                 let letterButton = UIButton(type: .system)
                 letterButton.titleLabel?.font = .systemFont(ofSize: 36)
                 letterButton.setTitle("WWW", for: .normal)
-                letterButton.layer.borderWidth = 1
-                letterButton.layer.borderColor = UIColor.black.cgColor
-
+                letterButton.addTarget(self, action: #selector(letterTapped), for: .touchUpInside)
                 let frame = CGRect(x: column * width,
                                    y: row * height,
                                    width: width,
                                    height: height)
                 letterButton.frame = frame
-
+                
                 buttonsView.addSubview(letterButton)
                 letterButtons.append(letterButton)
             }
-         }
-        
-//        cluesLabel.backgroundColor = .red
-//        answersLabel.backgroundColor = .blue
-//        buttonsView.backgroundColor = .brown
+        }
         
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        loadLevel()
     }
     
+    @objc func letterTapped(_ sender: UIButton) {
+        guard let buttonTitle = sender.titleLabel?.text else { return }
+        
+        currentAnswer.text = currentAnswer.text?.appending(buttonTitle)
+        activatedButtons.append(sender)
+        sender.isHidden = true
+    }
     
+    @objc func submitTapped(_ sender: UIButton) {
+        guard let answerText = currentAnswer.text else { return }
+        guard let solutionPosition = solutions.firstIndex(of: answerText) else { return }
+        
+        activatedButtons.removeAll()
+        
+        var splitAnswers = answersLabel.text?.components(separatedBy: "\n")
+        splitAnswers?[solutionPosition] = answerText
+        answersLabel.text = splitAnswers?.joined(separator: "\n")
+        
+        currentAnswer.text = ""
+        score += 1
+        
+        if score % 7 == 0 {
+            showNextLevelAlert()
+        }
+        
+    }
+    
+    func showNextLevelAlert() {
+        let alertController = UIAlertController(
+            title: "Well done!",
+            message: "Are you ready for the next level?",
+            preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(
+                                    title: "Let's go!",
+                                    style: .default,
+                                    handler: levelUp))
+        
+        present(alertController, animated: true)
+    }
+    
+    func levelUp(alertAction: UIAlertAction) {
+        level += 1
+        
+        solutions.removeAll(keepingCapacity: true)
+        letterButtons.forEach { $0.isHidden = false }
+        
+        loadLevel()
+    }
+    
+    @objc func clearTapped(_ sender: UIButton) {
+        currentAnswer.text = ""
+        
+        activatedButtons.forEach { $0.isHidden = false }
+        activatedButtons.removeAll()
+    }
+    
+    func loadLevel() {
+        guard
+            let levelFileURL = Bundle.main.url(forResource: "level\(level)", withExtension: "txt"),
+            let levelContents = try? String(contentsOf: levelFileURL)
+        else { return }
+        
+        var clueString = ""
+        var answersString = ""
+        var letterBits = [String]()
+        var lines = levelContents.components(separatedBy: "\n")
+        
+        lines.shuffle()
+        
+        for (index, line) in lines.enumerated() {
+            let parts = line.components(separatedBy: ": ")
+            let answerIncludedPipeSymbol = parts[0]
+            let clue = parts[1]
+            
+            clueString += "\(index + 1). \(clue)\n"
+            
+            let answerExcludedPipeSymbol = answerIncludedPipeSymbol.replacingOccurrences(of: "|", with: "")
+            solutions.append(answerExcludedPipeSymbol)
+            answersString += "\(answerExcludedPipeSymbol.count) letters\n"
+            
+            let answerLetterBits = answerIncludedPipeSymbol.components(separatedBy: "|")
+            letterBits += answerLetterBits
+        }
+        
+        cluesLabel.text = clueString.trimmingCharacters(in: .whitespacesAndNewlines)
+        answersLabel.text = answersString.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        letterButtons.shuffle()
+        
+        if letterButtons.count == letterBits.count {
+            for i in 0..<letterButtons.count {
+                letterButtons[i].setTitle(letterBits[i], for: .normal)
+            }
+        } else {
+            fatalError("level\(level)에 답이되는 word 갯수에 문제가 있다오 딱 20개가 되야 하는데 그걸 만족 못하고있다.")
+        }
+    }
 }
 
