afbca69cf53dbb25361c5ed2cb823496604c01c0
hongPro-ios <hongjeongseob@icloud.com>
2021-07-22 18:53:29 +0900

    Backup Project1 original

---
+++ Project1 backup/Project1/DetailViewController.swift
@@ -0,0 +1,52 @@
+//
+//  DetailViewController.swift
+//  Project1
+//
+//  Created by JEONGSEOB HONG on 2021/06/25.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    var selectedImage: String?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = selectedImage
+        
+        navigationItem.largeTitleDisplayMode = .never
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareApp))
+        
+        if let imageToLoad = selectedImage {
+            DispatchQueue.global(qos: .userInitiated).async { [weak self] in
+                let loadedImage = UIImage(named: imageToLoad)
+                DispatchQueue.main.async {
+                    self?.imageView.image = loadedImage
+                }
+            }
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+    
+   @objc func shareApp() {
+    let items: [Any] = [URL(string: "https://www.apple.com")!]
+    let ac = UIActivityViewController(activityItems: items, applicationActivities: nil)
+    
+    ac.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+    
+    present(ac, animated: true)
+    }
+}
---
+++ Project1/Project1/ImageCollectionViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by JEONGSEOB HONG on 2021/06/24.
+//
+
+import UIKit
+
+class ImageCollectionViewController: UICollectionViewController {
+    var pictures = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Storm Viewer"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        performSelector(inBackground: #selector(fetchPictures), with: self)
+    }
+    
+    @objc func fetchPictures() {
+        let fileManager = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fileManager.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
+        pictures.sort()
+        
+        DispatchQueue.main.async { [weak self] in
+            self?.collectionView.reloadData()
+        }
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        pictures.count
+    }
+        
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ImageCell", for: indexPath) as? ImageCollectionViewCell else { fatalError() }
+        let imageName = pictures[indexPath.item]
+
+        cell.configure(imageName: imageName)
+        
+        return cell
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        guard let detailViewController = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController else {
+            fatalError()
+        }
+        detailViewController.selectedImage = pictures[indexPath.item]
+        navigationController?.pushViewController(detailViewController, animated: true)
+        
+    }
+}
