4959995835ba878363d7698ce3bfa12f485396bd
hongPro-ios <hongjeongseob@icloud.com>
2021-08-03 18:20:33 +0900

    Add Project 14, part two about "SKAction"
    - [x]  Record your own voice saying "Game over!" and have it play when the game ends.
    - [x]  When showing "Game Over" add an SKLabelNode showing their final score
    - [ ] TBD: Use SKEmitterNode to create a smoke-like effect when penguins are hit, and a separate mud-like effect when they go into or come out of a hole

--- Project14/Project14/GameScene.swift
+++ Project14/Project14/GameScene.swift
@@ -13,16 +13,21 @@ enum Display {
     static let height = 768
 }
 
+enum SettingOption {
+    static let MAXRound = 20
+}
+
 class GameScene: SKScene {
     var slots = [WhackSlot]()
-    var gameScore: SKLabelNode!
+    var gameScore = GameScoreLabel(at: CGPoint(x: 8, y: 8))
     var score = 0 {
         didSet {
             gameScore.text = "Score: \(score)"
         }
     }
-    
     var popupTime = 0.85
+    var numRounds = 0
+    
     
     override func didMove(to view: SKView) {
         let background = SKSpriteNode(imageNamed: "whackBackground")
@@ -31,11 +36,6 @@ class GameScene: SKScene {
         background.zPosition = -1
         addChild(background)
         
-        gameScore = SKLabelNode(fontNamed: "Chalkduster")
-        gameScore.text = "Score: 0"
-        gameScore.position = CGPoint(x: 8, y: 8)
-        gameScore.horizontalAlignmentMode = .left
-        gameScore.fontSize = 48
         addChild(gameScore)
         
         for i in 0..<5 { createSlot(at: CGPoint(x:  100 + (i * 170), y: 410)) }
@@ -43,13 +43,43 @@ class GameScene: SKScene {
         for i in 0..<5 { createSlot(at: CGPoint(x:  100 + (i * 170), y: 230)) }
         for i in 0..<4 { createSlot(at: CGPoint(x:  180 + (i * 170), y: 140)) }
         
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1) { [weak self] in
-            self?.createEnemy()
-        }
+        startGame()
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+        let tappedNodes = nodes(at: location)
         
+        for node in tappedNodes {
+            guard let whackSlot = node.parent?.parent as? WhackSlot else { continue } // It gets the parent of the parent of the node, and typecasts it as a WhackSlot. This line is needed because the player has tapped the penguin sprite node, not the slot – we need to get the parent of the penguin, which is the crop node it sits inside, then get the parent of the crop node, which is the WhackSlot object, which is what this code does.
+            
+            if !whackSlot.isVisible { continue }
+            if whackSlot.isHit { continue }
+            
+            whackSlot.hit()
+            
+            if node.name == "charFriend" {
+                // they shouldn't have whacked this penguin
+                score -= 5
+                
+                run(SKAction.playSoundFileNamed("whackBad.caf", waitForCompletion: false))
+            } else if node.name == "charEnemy" {
+                // they should have whacked this one
+                whackSlot.charNode.xScale = 0.85
+                whackSlot.charNode.yScale = 0.85
+                
+                score += 1
+                
+                run(SKAction.playSoundFileNamed("whack.caf", waitForCompletion: false))
+            }
+        }
+    }
+    
+    func startGame() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) { [weak self] in
+            self?.createEnemy()
+        }
     }
     
     func createSlot(at position: CGPoint) {
@@ -61,11 +91,29 @@ class GameScene: SKScene {
     }
     
     func createEnemy() {
+        numRounds += 1
+        if numRounds >= SettingOption.MAXRound {
+            for slot in slots {
+                slot.hide()
+            }
+            
+            let gameOver = SKSpriteNode(imageNamed: "gameOver")
+            gameOver.position = CGPoint(x: 512, y: 384)
+            gameOver.zPosition = 1
+            run(SKAction.playSoundFileNamed("gameOver.m4a", waitForCompletion: false))
+            addChild(gameOver)
+            
+            let finalScoreLabel = FinalScoreLabel()
+            finalScoreLabel.text = "Final Score: \(score)"
+            addChild(finalScoreLabel)
+            
+            return
+        }
         popupTime *= 0.991
         
         slots.shuffle()
         slots[0].show(hideTime: popupTime)
-            
+        
         if Int.random(in: 0...12) > 4 { slots[1].show(hideTime: popupTime) }
         if Int.random(in: 0...12) > 8 { slots[2].show(hideTime: popupTime) }
         if Int.random(in: 0...12) > 10 { slots[3].show(hideTime: popupTime) }
@@ -79,4 +127,5 @@ class GameScene: SKScene {
             self?.createEnemy()
         }
     }
+    
 }
--- README.md
+++ README.md
@@ -167,7 +167,8 @@ The next three projects we’ll be making move into the realm of richer user int
  - 20210801 메모: https://www.notion.so/54day-96d68955564f486c858c8ec6736ceced
 - [x] ~~Day55 – Project 14, part one~~
  - 20210802 메모: https://www.notion.so/55day-bc0c6fa13e6b47dc9df9aa5a1a9b019b
-- [ ] Day56 – Project 14, part two
+- [x] ~~Day56 – Project 14, part two~~
+ - 20210803 메모: https://www.notion.so/56day-235d0eecd87f4a129df862d8a025f786
 - [ ] Day57 – Project 15, part one
 - [ ] Day58 – Project 15, part two
  
