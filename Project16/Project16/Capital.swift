db31b2771577b8d385be2ae0a8e48068d6cf8e4b
hongPro-ios <hongjeongseob@icloud.com>
2021-08-07 21:15:31 +0900

    Add Project 16, part one about "MapKit"
    - Usage of Annotation
    - Usage of Annotation's accessory and accessory's tap action

---
+++ Project16/Project16/Capital.swift
@@ -0,0 +1,22 @@
+//
+//  Capital.swift
+//  Project16
+//
+//  Created by JEONGSEOB HONG on 2021/08/07.
+//
+
+import MapKit
+import UIKit
+
+class Capital: NSObject, MKAnnotation {
+    var title: String?
+    var coordinate: CLLocationCoordinate2D
+    var info: String
+    
+    init(title: String, coordinate: CLLocationCoordinate2D, info: String) {
+        self.title = title
+        self.coordinate = coordinate
+        self.info = info
+    }
+
+}
